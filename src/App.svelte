7722b2fc44001ef2bb1fc0ae26689e63966eff35
JP Barringer <jp@trapeze.io>
2025-07-24 14:41:37 -0400

    Initial commit. thimblesoft github page

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import './app.css'
+</script>
+
+<main class="min-h-screen bg-gray-50 flex items-center justify-center">
+  <div class="text-center px-6">
+    <h1 class="text-5xl font-bold text-gray-900 mb-4">Thimblesoft</h1>
+    <p class="text-xl text-gray-600">Coming Soon</p>
+  </div>
+</main>
